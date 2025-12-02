e3d2c1efc48509d8109d50fc78b2b157211ef94c
Goge <hamidsiddiqi888@gmail.com>
2025-12-02 16:12:48 +0500

    first commit

--- src/components/FiverrWatermark.tsx
+++ src/components/FiverrWatermark.tsx
@@ -4,7 +4,7 @@ export default function FiverrWatermark() {
     return (
         <div className={styles.watermark}>
             <a
-                href="https://www.fiverr.com"
+                href="https://www.fiverr.com/hamidsiddiqii"
                 target="_blank"
                 rel="noopener noreferrer"
                 className={styles.link}
@@ -16,7 +16,7 @@ export default function FiverrWatermark() {
                 >
                     <path d="M23 11.5c0 6.351-5.149 11.5-11.5 11.5S0 17.851 0 11.5 5.149 0 11.5 0 23 5.149 23 11.5zm-2.3 0c0-5.082-4.118-9.2-9.2-9.2s-9.2 4.118-9.2 9.2 4.118 9.2 9.2 9.2 9.2-4.118 9.2-9.2zM7.432 8.626h1.013v1.014H7.432V8.626zm5.485 0h1.013v1.014h-1.013V8.626zm-2.742 0h1.013v1.014h-1.013V8.626zm4.118 2.742h-1.013v4.118h1.013v-4.118zm-2.742 0h-1.013v4.118h1.013v-4.118zm-2.743 0H7.795v4.118h1.013v-4.118z" />
                 </svg>
-                <span className={styles.text}>Made on Fiverr</span>
+                <span className={styles.text}>Made on Fiverr by Hamid Siddiqi</span>
             </a>
         </div>
     );
